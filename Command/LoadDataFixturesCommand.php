41f1a74e37adc9859a53acd12b0062ec4c324bf2
Jon Smith <smithj@thelazysysadmin.net>
2013-08-15 13:15:08 +1000

    Fix indentation issues

--- Command/LoadDataFixturesCommand.php
+++ Command/LoadDataFixturesCommand.php
@@ -11,13 +11,13 @@ use Symfony\Component\Console\Input\ArrayInput;
 
 class LoadDataFixturesCommand extends DoctrineCommand
 {
-	protected function configure()
-	{
-		$this
-			->setName("doctrine:fixtures:resetload")
-			->setDescription("Reset autonumbering with MySQL and then Load Data Fixtures")
+    protected function configure()
+    {
+        $this
+            ->setName("doctrine:fixtures:resetload")
+            ->setDescription("Reset autonumbering with MySQL and then Load Data Fixtures")
             ->addOption('em', null, InputOption::VALUE_REQUIRED, 'The entity manager to use for this command.');
-	}
+    }
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
@@ -32,7 +32,7 @@ class LoadDataFixturesCommand extends DoctrineCommand
 
         $purger->purge();
 
-		$metadatas = $em->getMetadataFactory()->getAllMetadata();
+        $metadatas = $em->getMetadataFactory()->getAllMetadata();
         foreach ($metadatas as $metadata) {
             if (!$metadata->isMappedSuperclass) {
                 $tbl = $metadata->getQuotedTableName($platform);
@@ -41,19 +41,19 @@ class LoadDataFixturesCommand extends DoctrineCommand
             }
         }
 
-		$command = $this->getApplication()->find('doctrine:fixtures:load');
+        $command = $this->getApplication()->find('doctrine:fixtures:load');
 
-		// Using the append option as data has already been purged from the database
-	    $arguments = array(
-			'command' => 'doctrine:fixtures:load',
-	        '--append'  => true,
-	    );
+        // Using the append option as data has already been purged from the database
+        $arguments = array(
+            'command' => 'doctrine:fixtures:load',
+            '--append'  => true,
+        );
 
-	    if ($input->getOption('em')) {
-	    	$arguments['--em'] = $input->getOption('em');
-	    }
+        if ($input->getOption('em')) {
+            $arguments['--em'] = $input->getOption('em');
+        }
 
-	    $inputs = new ArrayInput($arguments);
-	    $returnCode = $command->run($inputs, $output);
+        $inputs = new ArrayInput($arguments);
+        $returnCode = $command->run($inputs, $output);
     }
 }
